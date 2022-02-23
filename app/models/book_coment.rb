2f9d97222dcb8add7c3edef5ffe84bb02488490a
EC2 Default User <ec2-user@ip-172-31-94-108.ec2.internal>
2022-02-23 09:42:15 +0000

    [Finish] 応用課題3

---
+++ app/models/book_coment.rb
@@ -0,0 +1,6 @@
+class BookComent < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+  
+  validates
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+end
